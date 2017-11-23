134d057d7ddd155c21ffb9624add46da21f1842d
Richard Ellery <raellery@gmail.com>
2017-11-23 21:04:08 +0900

    Size ripple same as button

--- scripts.js
+++ scripts.js
@@ -8,5 +8,9 @@ function createRipple (e) {
   var circle = document.createElement('div');
   this.appendChild(circle);
   
+  var d = Math.max(this.clientWidth, this.clientHeight);
+  
+  circle.style.width = circle.style.height = d + 'px';
+  
   circle.classList.add('ripple');
 }
