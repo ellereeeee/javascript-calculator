9c6cfa9a6f8b7e1b8df03159318c6ab70e8b4f18
Richard Ellery <raellery@gmail.com>
2017-11-23 20:21:39 +0900

    Create ripple-making function

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ var buttons = document.getElementsByTagName('button');
 Array.prototype.forEach.call(buttons, function (b) {
   b.addEventListener('click', createRipple);
 });
+
+function createRipple (e) {
+  var circle = document.createElement('div');
+  this.appendChild(circle);
+  
+  circle.classList.add('ripple');
+}
