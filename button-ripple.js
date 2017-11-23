dd8d5addfe1b24ce6d36c7b48b0e59fbeeee7287
Richard Ellery <raellery@gmail.com>
2017-11-23 22:34:39 +0900

    Add comments to button-ripple.js

--- button-ripple.js
+++ button-ripple.js
@@ -1,8 +1,11 @@
-var buttons = document.getElementsByTagName('button');
+/* This code is from https://github.com/GeekLaunch/button-ripple-effect
+I studied the tutorial and incorporated it into my code. */
+
+var buttons = document.getElementsByTagName('button'); // put buttons into array
 
 Array.prototype.forEach.call(buttons, function (b) {
   b.addEventListener('click', createRipple);
-});
+}); // add click event called "createRipple" for each button
 
 function createRipple (e) {
   var circle = document.createElement('div');
@@ -16,4 +19,7 @@ function createRipple (e) {
   circle.style.top = e.clientY - this.offsetTop - d/2 + 'px';
   
   circle.classList.add('ripple');
-}
+} /* Make element called "circle" with class of "ripple."
+Make dimensions of circle equal to the higher of the button width of height.
+Make the circle originate from the origin of the click, accounting for
+the offset of the buttons parent element in the display. */
