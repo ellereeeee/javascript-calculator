65ffb9c98f8e4ce167f956c8a9de382068411d19
Richard Ellery <raellery@gmail.com>
2017-12-10 16:21:57 +0900

    Add comments to key_type object

--- calculator.js
+++ calculator.js
@@ -1,10 +1,10 @@
 // Having a number for each key type helps ensure the right function is executed when a button is pressed.
 var key_type = {
-  NUM: 1,
-  OP_KEY: 2,
-  PERC: 3,
-  REV: 4,
-  CLEAR: 5,
-  DEC: 6,
-  EQUALS: 7
+  NUM: 1, // numbers
+  OP_KEY: 2, // operator
+  PERC: 3, // percent key
+  REV: 4, // change to positive / negative key
+  CLEAR: 5, // all clear key
+  DEC: 6, // decimal
+  EQUALS: 7 // equals
 }
