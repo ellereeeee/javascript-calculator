9882d2ab34dc8b0b3458bda45028c3155d4673c3
Richard Ellery <raellery@gmail.com>
2017-12-10 16:19:30 +0900

    Add key_type object

---
+++ calculator.js
@@ -0,0 +1,10 @@
+// Having a number for each key type helps ensure the right function is executed when a button is pressed.
+var key_type = {
+  NUM: 1,
+  OP_KEY: 2,
+  PERC: 3,
+  REV: 4,
+  CLEAR: 5,
+  DEC: 6,
+  EQUALS: 7
+}
